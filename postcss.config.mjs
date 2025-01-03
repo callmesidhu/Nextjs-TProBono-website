5e579daaa348a7bb09bf886552990fc09574722f
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-03 21:30:27 +0530

    prediction bug fix deploy

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: { 
-    'tailwindcss': {},
-    'postcss-preset-env': {},
-    'autoprefixer': {},
+  plugins: {
+    tailwindcss: {},
   },
 };
 
