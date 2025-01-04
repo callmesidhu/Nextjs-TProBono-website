26a5b510ad7350e77d0e68a0f679b2367ce78c2c
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-04 23:45:58 +0530

    loader added

---
+++ src/Components/Loader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Loader.css';
+import Image from "next/image";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="splash-screen p-5 pl-10">
+    <Image 
+  src="/Images/logo.png" 
+  alt="TProBono Logo" 
+  width={150} 
+  height={150} 
+/>
+      <div className="spinner" />
+    </div>
+  );
+};
+
+export default Loader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
-
+import React, { useState, useEffect } from "react";
 import About from "@/Components/About";
 import Contact from "@/Components/Contact";
 import Intro from "@/Components/Intro";
 import Navbar from "@/Components/Navbar";
 import Projects from "@/Components/Projects";
 import Services from "@/Components/Services";
+import Loader from "@/Components/Loader";
+
 
 
 export default function Home() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return isLoading?(<Loader/>):(
     <div className="App flex-col flex pb-4">
       <section className="lgbg1">
       <Navbar />
