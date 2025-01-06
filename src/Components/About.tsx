5824f3f75594f66c5543a47cd9b4bfc404d75441
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-06 18:29:22 +0530

    deploy bug

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from 'react';
+import React from 'react';
 import './About.css';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';  
